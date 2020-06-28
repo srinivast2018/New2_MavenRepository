d5cddd14dd4e2041985ec7792c663ba97e48add8
srinivast2018 <srinivas.nirmal@gmail.com>
2020-06-28 13:39:58 +0530

    first committ

---
+++ src/main/java/com/automation/new/App.java
@@ -0,0 +1,13 @@
+package com.automation.new;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
